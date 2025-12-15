570d19a98308dc51777666f558474ef4be73922b
RakaValrizqy <pendragonqword@gmail.com>
2025-12-15 22:51:17 +0700

    menambahkan controller level

---
+++ backend/express_logilearn/src/controllers/levelController.js
@@ -0,0 +1,79 @@
+const Level = require('../models/level')
+const response = require('../helpers/response')
+const { levels } = require('../models/prisma')
+
+async function create(req, res) {
+    try {
+        const {name, idSection} = req.body
+        const data = await Level.createLevel(idSection, name)
+        response(200, data, `successfully`, res)
+    } catch(err) {
+        console.log(err.message)
+        response(500, null, `failed to : ${err.message}`, res)
+    }
+}
+
+async function getAll(req, res) {
+    try {
+        const data = await Level.getAllLevels()
+        if(!data){
+            return response(404, null, `data not found`, res)
+        }
+        response(200, data, `get all levels`, res)
+    } catch(err) {
+        console.log(err.message)
+        response(500, null, `failed to : ${err.message}`, res)
+    }
+}
+
+async function getById(req, res) {
+    try {
+        const {id} = req.params
+        const data = await Level.getLevelById(id)
+        if(!data){
+            return response(404, null, `data not found`, res)
+        }
+        response(200, data, `get level by id: ${id}`, res)
+    } catch(err) {
+        console.log(err.message)
+        response(500, null, `failed to get level by id: ${err.message}`, res)
+    }
+}
+
+async function update(req, res) {
+    try {
+        const {id} = req.params
+        const {name, idSection} = req.body
+        const data = await Level.getLevelById(id)
+        if(!data){
+            return response(404, null, `data not found`, res)
+        }
+        const updated = await Level.updateLevel(id, idSection, name)
+        response(200, updated, `level updated successfully`, res)
+    } catch(err) {
+        console.log(err.message)
+        response(500, null, `failed to update level: ${err.message}`, res)
+    }
+}
+
+async function remove(req, res) {
+    try {
+        const {id} = req.params
+        const data = await Level.getLevelById(id)
+        if(!data){
+            return response(404, null, `data not found`, res)
+        }
+        await Level.deleteLevel(id)
+        response(200, null, `level deleted successfully`, res)
+    } catch(err) {
+        console.log(err.message)
+        response(500, null, `failed to delete level: ${err.message}`, res)
+    }
+}
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
