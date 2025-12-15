1ae0e868c94bbf6ea2d2bcb262aeadae958a67e9
RakaValrizqy <pendragonqword@gmail.com>
2025-12-15 22:30:58 +0700

    menambahkan error handling ketika data sections tidak ditemukan

--- backend/express_logilearn/src/controllers/sectionController.js
+++ backend/express_logilearn/src/controllers/sectionController.js
@@ -4,6 +4,9 @@ const response = require('../helpers/response')
 async function getAll(req, res) {
     try {
         const data = await Section.getAllSections()
+        if (!data){
+            return response(404, null, `data not found`, res)
+        }
         response(200, data, `get all sections`, res)
     } catch (err) {
         console.log(err.message)
@@ -15,6 +18,9 @@ async function getById(req, res) {
     try {
         const {id} = req.params
         const data = await Section.getSectionById(id)
+        if (!data){
+            return response(404, null, `data not found`, res)
+        }
         response(200, data, `get section by id: ${id}`, res)
     } catch (err) {
         response(500, null, `failed to get section by id: ${err.message}`, res)
@@ -25,7 +31,6 @@ async function create(req, res) {
     try {
         const {name} = req.body
         const data = await Section.createSection(name)
-        console.log(data)
         response(200, data, `section created successfully`, res)
     } catch (err){
         console.log(err.message)
@@ -37,9 +42,12 @@ async function update(req, res) {
     try {
         const {id} = req.params
         const {name} = req.body
-        const data = await Section.updateSection(id,name)
-        console.log(data)
-        response(200, data, `section updated successfully`, res)
+        const data = await Section.getSectionById(id)
+        if (!data){
+            return response(404, null, `data not found`, res)
+        }
+        const updated = await Section.updateSection(id,name)
+        response(200, updated, `section updated successfully`, res)
     } catch (err){
         console.log(err.message)
         response(500, null, `failed to update section: ${err.message}`, res)
@@ -49,9 +57,12 @@ async function update(req, res) {
 async function remove(req, res) {
     try {
         const {id} = req.params
-        const data = await Section.deleteSection(id)
-        console.log(data)
-        response(200, data, `section deleted successfully`, res)
+        const data = await Section.getSectionById(id)
+        if (!data){
+            return response(404, null, `data not found`, res)
+        }
+        await Section.deleteSection(id)
+        response(200, null, `section deleted successfully`, res)
     } catch (err) {
         console.log(err.message)
         response(500, null, `failed to delete section: ${err.message}`, res)
