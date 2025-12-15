47ec11ce2c9f7ea3e2e86fe1c2255727cc507352
RakaValrizqy <pendragonqword@gmail.com>
2025-12-15 22:19:36 +0700

    melengkapi crud section

--- backend/express_logilearn/src/controllers/sectionController.js
+++ backend/express_logilearn/src/controllers/sectionController.js
@@ -13,8 +13,9 @@ async function getAll(req, res) {
 
 async function getById(req, res) {
     try {
+        const {id} = req.params
         const data = await Section.getSectionById(id)
-        response(200, data, `get section by id`, res)
+        response(200, data, `get section by id: ${id}`, res)
     } catch (err) {
         response(500, null, `failed to get section by id: ${err.message}`, res)
     }
@@ -32,8 +33,35 @@ async function create(req, res) {
     }
 }
 
+async function update(req, res) {
+    try {
+        const {id} = req.params
+        const {name} = req.body
+        const data = await Section.updateSection(id,name)
+        console.log(data)
+        response(200, data, `section updated successfully`, res)
+    } catch (err){
+        console.log(err.message)
+        response(500, null, `failed to update section: ${err.message}`, res)
+    }
+}
+
+async function remove(req, res) {
+    try {
+        const {id} = req.params
+        const data = await Section.deleteSection(id)
+        console.log(data)
+        response(200, data, `section deleted successfully`, res)
+    } catch (err) {
+        console.log(err.message)
+        response(500, null, `failed to delete section: ${err.message}`, res)
+    }
+}
+
 module.exports = {
     getAll,
     getById,
-    create
+    create,
+    update,
+    remove
 }
--- backend/express_logilearn/src/routes/sectionRoutes.js
+++ backend/express_logilearn/src/routes/sectionRoutes.js
@@ -2,9 +2,17 @@ const express = require('express')
 const router = express.Router()
 const sectionController = require('../controllers/sectionController')
 
+//create
+router.post('/sections', sectionController.create)
+
+//read
 router.get('/sections', sectionController.getAll)
 router.get('/sections/:id', sectionController.getById)
 
-router.post('/sections', sectionController.create)
+//update
+router.put('/sections/:id', sectionController.update)
+
+//delete
+router.delete('/sections/:id', sectionController.remove)
 
 module.exports = router
