c5c08fc5f44a2631708ab074052869f15c9db998
Naufal Fahreza <naufalfahreza328@gmail.com>
2025-12-19 21:37:03 +0700

    menambahkan page add dan edit soal esai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import DetailAttempt from './pages/DetailAttempt';
 import AddSoalPG from './pages/add/AddSoalPG';
 import ListSoalPG from './pages/ListSoalPG';
 import DetailSoalPG from './pages/DetailSoalPG';
+import AddSoalEsai from './pages/add/AddSoalEsai';
+import EditSoalEsai from './pages/edit/EditSoalEsai';
 
 
 function App() {
@@ -28,6 +30,10 @@ function App() {
         <Route path="/soals-pg" element={<ListSoalPG />} />
         <Route path="/soals-pg/:id" element={<DetailSoalPG />} />
         <Route path="/add-soal-pg" element={<AddSoalPG />} />
+
+        {/* 6. Soal Essai */}
+        <Route path="/add-esai" element={<AddSoalEsai />} />
+        <Route path="/edit-esai/:id" element={<EditSoalEsai />} />
 
         {/* 4. Review Attempt */}
         <Route path="/review-attempt" element={<ReviewAttempt />} />
