f0b7786d5c72b74552c429c3891189ad22fac87c
RakaValrizqy <pendragonqword@gmail.com>
2025-12-19 20:38:03 +0700

    implementasi pembagian hak akses untuk pelajar dan admin

--- backend/express_logilearn/src/routes/authRoutes.js
+++ backend/express_logilearn/src/routes/authRoutes.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-const { loginAdmin } = require('../controllers/authController');
+const authController = require('../controllers/authController');
+const authMid = require('../middlewares/authMiddleware')
 const { verifyLogin } = require('../middlewares/authMiddleware');
 const resp = require('../helpers/response');
 
-router.post('/login-admin', loginAdmin);
+router.post('/login-admin', authController.loginAdmin);
+router.post('/login-pelajar', authController.loginPelajar);
 
-router.get('/test-login-admin', verifyLogin, (req, res) => {
+router.get('/test-login-admin', authMid.verifyLogin, authMid.onlyAdmin, (req, res) => {
   if (!req.auth) {
     return resp(401, null, 'Unauthorized', res);
   }
 
-  if (req.auth.type !== 'ADMIN') {
-    return resp(403, null, 'Admin only', res);
+  return resp(200, null, 'admin profile', res);
+});
+
+router.get('/test-login-pelajar', verifyLogin, (req, res) => {
+  if (!req.auth) {
+    return resp(401, null, 'Unauthorized', res);
   }
 
-  return resp(200, null, 'admin profile', res);
+  return resp(200, null, 'pelajar profile', res);
 });
 
 module.exports = router;
